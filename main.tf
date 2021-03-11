c7e245a0d2d1a6ce8af0b622b6b417e124b92a6a
Robin Norwood <rnorwood@hashicorp.com>
2021-03-11 14:07:05 -0600

    Remove SNS from example

--- main.tf
+++ main.tf
@@ -29,47 +29,6 @@ module "s3_bucket" {
   acl    = "private"
 }
 
-module "sns_topic" {
-  depends_on = [module.s3_bucket]
-  source     = "terraform-aws-modules/sns/aws"
-
-  name = "my-topic"
-}
-
-resource "aws_sns_topic_policy" "default" {
-  arn = module.sns_topic.this_sns_topic_arn
-
-  policy = <<EOF
-{
-    "Version":"2012-10-17",
-    "Statement":[{
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "s3.amazonaws.com"
-        },
-        "Action": "SNS:Publish",
-        "Resource": "${module.sns_topic.this_sns_topic_arn}",
-        "Condition":{
-            "ArnLike":{"aws:SourceArn":"${module.s3_bucket.this_s3_bucket_arn}"}
-        }
-    }]
-}
-EOF
-}
-
-resource "aws_s3_bucket_notification" "notif" {
-  bucket = module.s3_bucket.this_s3_bucket_id
-
-  topic {
-    topic_arn = module.sns_topic.this_sns_topic_arn
-
-    events = [
-      "s3:ObjectCreated:*",
-      "s3:ObjectRemoved:*"
-    ]
-  }
-}
-
 resource "random_pet" "object_names" {
   count = 4
 
